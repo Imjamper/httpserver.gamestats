d175610f8b065603c66170dc855486a2e1646536
thejackgatsby <Imjamper@gmail.com>
2017-02-22 02:55:37 +0400

    UnitTests, FirstTest, NUnit, NSubstitute

---
+++ GL.HttpServer.UnitTests/Cache/MemoryCacheTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using GL.HttpServer.Cache;
+using NUnit.Framework;
+
+namespace GL.HttpServer.UnitTests.Cache
+{
+    [TestFixture]
+    public class MemoryCacheTests
+    {
+        [Test]
+        public void TryGetValue_AddNewItem_ReturnSameData()
+        {
+            var inputCacheItem = new List<string>();
+            inputCacheItem.Add("SomeData");
+            MemoryCache.Cache<List<string>>().AddOrUpdate("TestKey", inputCacheItem);
+            Thread.Sleep(5000);
+            List<string> returnCacheItem;
+            MemoryCache.Cache<List<string>>().TryGetValue("TestKey", out returnCacheItem);
+            Assert.AreEqual(inputCacheItem, returnCacheItem);
+        }
+    }
+}
---
+++ Kontur.GameStats.Server.UnitTests/HttpServices/ReportsServiceTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Kontur.GameStats.Server.UnitTests.HttpServices
+{
+    [TestFixture]
+    public class ReportsServiceTests
+    {
+        [Test]
+        public void RecentMatches()
+        {
+        }
+    }
+}
