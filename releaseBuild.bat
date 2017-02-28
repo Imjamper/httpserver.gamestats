b32a68fab422d8c758c2f0c1c46e4bd00d72ca68
thejackgatsby <Imjamper@gmail.com>
2017-03-01 00:55:13 +0400

    HttpClientBuild

--- releaseBuild.bat
+++ releaseBuild.bat
@@ -1,5 +1,6 @@
 @echo off
 "Tools\nuget.exe" restore "Kontur.GameStats.sln" 
 "Tools\msbuild.exe" "Kontur.GameStats.sln" /t:Build /p:Configuration=Release
+"Tools\msbuild.exe" "HttpClient\HttpClient.sln" /t:Build /p:Configuration=Release
 "Tools\nUnit3\nunit3-console.exe" --noresult "GL.HttpServer.UnitTests\GL.HttpServer.UnitTests.csproj" /config:Release
 "Tools\nUnit3\nunit3-console.exe" --noresult "Kontur.GameStats.Server.UnitTests\Kontur.GameStats.Server.UnitTests.csproj" /config:Release
